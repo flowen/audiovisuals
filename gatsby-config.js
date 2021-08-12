b38df9ba46ff0bef93e9d5d5aaeed60a5ad48938
Rou Hun <lowen.f@gmail.com>
2021-08-12 16:49:03 +0200

    hello world

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `audiovisuals-r3f`,
+    description: `audiovisuals with react three fiber`,
+    author: `@flowen`,
+    siteUrl: `https://flowen.me`,
+  },
+  plugins: [
+  ],
+}
